f395653bff870717042206bf6a707870d7c37d01
almadounasyasmine0 <almadounasyasmine0@gmail.com>
2025-11-16 17:18:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,37 @@ Running this file should open a Pygame window showing a static "Main Menu" heade
 Close the window manually to exit.
 """
 from interface import menu
+from population import Population
+from interface import menu, run_play 
+from population import Population
 
 
+
+
+def main():
+    population_size = 50
+    
+    # 1. Création population initiale
+    population = Population(population_size)  
+    
+    while True:
+        # 2. Vérification des mouvements
+        population.check_population() 
+        
+        # 3. Évaluation
+        max_fitness, best_knight = population.evaluate() 
+        
+        # 4. Condition d'arrêt
+        if max_fitness == 64:
+            break
+        
+        # 5. Nouvelle génération
+        population.create_new_generation()  
+    
+    # 6. Affichage solution
+    run_play(best_knight)  
+    
 if __name__ == "__main__":
-	menu.run_menu()
+    # Lance le menu principal
+    menu.run_menu()
 
